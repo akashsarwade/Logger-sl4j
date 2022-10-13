a6a316375436aa2b0e497250d33a5e0e45c9936a
Akash Sarwade <akshsarwade@gmail.com>
2022-10-13 14:04:13 +0530

    secomd commit

--- src/main/java/com/logger/controller/MessageController.java
+++ src/main/java/com/logger/controller/MessageController.java
@@ -32,3 +32,24 @@ public class MessageController {
 		return responce;
 	}
 }
+
+//
+//
+//echo "# Logger-sl4j" >> README.md
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin https://github.com/akashsarwade/Logger-sl4j.git
+//git push -u origin main
+//
+//
+//
+//
+//
+//…or push an existing repository from the command line
+//git remote add origin https://github.com/akashsarwade/Logger-sl4j.git
+//git branch -M main
+//git push -u origin main
+//
+
